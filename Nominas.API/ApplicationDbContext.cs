90141704b6475f316dfcf33cae95b5b0290c0fc8
Rafael L <r4f4elzaragoza@gmail.com>
2022-03-29 22:50:17 +0200

    Add project files.

---
+++ Nominas.API/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Nominas.API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //public DbSet<Company> Companies { get; set; }
+        //public DbSet<Contract> Contracts { get; set; }
+        //public DbSet<Worker> Workers { get; set; }
+        //public DbSet<Payroll> Payrolls { get; set; }
+    }
+}
---
+++ Nominas.API/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace Nominas.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Nominas.API", Version = "v1" }); });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nominas.API v1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
